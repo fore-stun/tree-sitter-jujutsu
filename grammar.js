fore-stun/tree-sitter-alloy

Init grammar file

grammar.js
@@ -1,0 +1,8 @@
+module.exports = grammar({
+  name: "alloy",
+
+  rules: {
+    // TODO: add the actual grammar rules
+    source_file: ($) => "hello",
+  },
+});

fore-stun/tree-sitter-jujutsu

grammar.js
@@ -1,0 +1,8 @@
+module.exports = grammar({
+  name: "jujutsu",
+
+  rules: {
+    // TODO: add the actual grammar rules
+    source_file: ($) => "hello",
+  },
+});